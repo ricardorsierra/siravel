011713fbd36345562f0633dbcfb11a7cbc48b076
Ricardo R. Sierra <ricardo@ricardosierra.com.br>
2017-04-29 10:56:20 -0300

    Menu Lang

--- src/Services/Traits/LangServiceTrait.php
+++ src/Services/Traits/LangServiceTrait.php
@@ -32,13 +32,12 @@ trait LangServiceTrait
         $response = '<li>';
         $response .= '<a href=""><span class="'.$current['class'].'"></span></a>';
         $response .= '<ul class="sub-menu clearfix">';
-        $response .= '<a href="'.url('sitec/language/set/'.$current['locale']).'"><span class="no-translation current-lang menu-item '.$current['class'].'"></span></a>';
+        $response .= '<li class=\'no-translation menu-item current-lang \'><a href="'.url('sitec/language/set/'.$current['locale']).'"><span class="'.$current['class'].'"></span></a></li>';
 
         foreach ($langs as $lang) {
             if ($lang['locale'] !== $current['locale']) {
-                $response .= '<a href="'.url('sitec/language/set/'.$lang['locale']).'"><span class="no-translation menu-item '.$lang['class'].'"></span></a>';
+                $response .= '<li class=\'no-translation menu-item\'><a href="'.url('sitec/language/set/'.$lang['locale']).'"><span class="'.$lang['class'].'"></span></a></li>';
             }
-            $response .= '<li class=\'no-translation menu-item\'><a href=\''.url('sitec/language/set/'.$link['locale']).'\'><span class="flag-icon '.$link['class'].'"></span></a></li>';
         }
 
         $response .= '</ul></li>';
